4ca1686b05d8e0aa30a6f7ba628817e39e46b5fc
Oakley <dalford278@gmail.com>
2025-07-21 18:29:12 -0400

    building Pokedex foundation

---
+++ command_map.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type result struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type MapList struct {
+	Count    int      `json:"count"`
+	Next     string   `json:"next"`
+	Previous string   `json:"previous"`
+	Results  []result `json:"results"`
+}
+
+func commandMapf(cfg *config) error {
+	var url string
+	if cfg.NextURL == "" {
+		url = "https://pokeapi.co/api/v2/location-area/"
+	} else {
+		url = cfg.NextURL
+	}
+	res, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("failed to fetch map data: %w", err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		return fmt.Errorf("failed to read response body: %w", err)
+	}
+	var mapData MapList
+
+	if err := json.Unmarshal(body, &mapData); err != nil {
+		return fmt.Errorf("failed to decode map data: %w", err)
+	}
+
+	for _, area := range mapData.Results {
+		fmt.Println(area.Name)
+	}
+	cfg.NextURL = mapData.Next
+	cfg.PrevURL = mapData.Previous
+	return nil
+}
+
+func commandMapb(cfg *config) error {
+	if cfg.PrevURL == "" {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+	var url string = cfg.PrevURL
+	res, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("failed to fetch map data: %w", err)
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		return fmt.Errorf("failed to read response body: %w", err)
+	}
+	var mapData MapList
+
+	if err := json.Unmarshal(body, &mapData); err != nil {
+		return fmt.Errorf("failed to decode map data: %w", err)
+	}
+	for _, area := range mapData.Results {
+		fmt.Println(area.Name)
+	}
+	cfg.NextURL = mapData.Next
+	cfg.PrevURL = mapData.Previous
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Alford05/Pokedex
+
+go 1.23.5
